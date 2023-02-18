ac5485864cec8c01746319a3706dc283415f7619
Yeison Arenas <yeisonarenas177020@correo.itm.edu.co>
2023-02-18 00:44:19 -0500

    Create Database

---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} de tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+    }
+}
