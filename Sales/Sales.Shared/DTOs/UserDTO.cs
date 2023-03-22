f3ee0654798d2b53f029e0ce31650bafad8f7cdc
Yeison Arenas <yeisonarenas177020@correo.itm.edu.co>
2023-03-21 21:47:59 -0500

    Update UserDTO

--- Sales/Sales.Shared/DTOs/UserDTO.cs
+++ Sales/Sales.Shared/DTOs/UserDTO.cs
@@ -15,6 +15,7 @@ namespace Sales.Shared.DTOs
         [Display(Name = "Contraseña")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        
         public string Password { get; set; } = null!;
 
         [Compare("Password", ErrorMessage = "La contraseña y la confirmación no son iguales.")]
@@ -22,6 +23,7 @@ namespace Sales.Shared.DTOs
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        
         public string PasswordConfirm { get; set; } = null!;
     }
 
